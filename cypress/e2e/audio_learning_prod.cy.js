42711a81958f9a09d79ec36b873ae1b748bfdaec
Risna Zahira <risnazahir@student.telkomuniversity.ac.id>
2024-10-30 11:26:51 +0700

    update prod semua fitur

--- cypress/e2e/audio_learning_prod.cy.js
+++ cypress/e2e/audio_learning_prod.cy.js
@@ -8,7 +8,7 @@ describe('Login', () => {
         cy.clearLocalStorage();
     });
 
-    it('Tes Generate Soal Coqa', () => {
+    it('Tes Generate Audio Learning', () => {
         cy.visit('https://.marshall-teach.web.id');
 
         // login
@@ -50,5 +50,37 @@ describe('Login', () => {
 
         // Validasi hasil atau navigasi yang diharapkan setelah generate
         //cy.url().should('include', '/hasil-audio'); // validasi ke halaman hasil
+
+        //--------------------------------------------------------------------------------------------------
+        //2
+        // Pilih menu audio learning
+        // cy.get(':nth-child(7) > a > .fas', timeout).click();
+
+        // // Pilih output, pastikan menu sudah di-render dengan baik
+        // cy.get('[style="margin-bottom: 10px;"] > .Dropdown-root > .Dropdown-control').click();
+        // cy.get('.Dropdown-menu > :nth-child(2)').click();
+
+        // // Default testing dengan file PPTX
+        // cy.get('.drop-file-input > input', timeout).attachFile('[TC-102 Deep Learning]_Deeplearning.pptx');
+
+        // // Setting abstrak dulu
+        // cy.get('.checkBox_abstrakAudio', timeout).click();
+        // cy.get('.inputText_AbstrakPDF', timeout).clear().type('4500'); // set abstrak menjadi 4500
+
+        // // Pastikan tombol generate hanya diklik jika belum ada request aktif
+        // cy.get('.audioBut', timeout).should('not.be.disabled'); // pastikan tombol tidak disabled sebelum klik
+        // cy.screenshot(`inputAudioLearning-${new Date().toISOString().replace(/[:.]/g, '-')}`);
+
+        // // Cek jika request belum dikirim sebelumnya (hanya satu request yang dikirim)
+        // cy.intercept('POST', '/marshall-legacy/result-ebook-to-audio/').as('generateRequest');
+        
+        // // Tombol generate dan tunggu respons
+        // cy.get('.audioBut', timeout).click();
+        
+        // // Tunggu hingga respons selesai
+        // cy.wait('@generateRequest').then((interception) => {
+        //     expect(interception.response.statusCode).to.eq(200); // pastikan respons sukses
+        // });
+
     });
 });
--- cypress/e2e/coqa_prod.cy.js
+++ cypress/e2e/coqa_prod.cy.js
@@ -1,72 +1,129 @@
-var timeout = { timeout: 60000 };
-var force = { force: true };
+var timeout = { timeout: 60000 }; // Memperpanjang timeout menjadi 60 detik untuk semua elemen yang mungkin lambat
 import 'cypress-file-upload';
 
 describe('Login', () => {
-  beforeEach(() => {
-    // Clear cookies and local storage before each test
+  before(() => {
+    // Membersihkan cookies dan local storage sebelum memulai tes
     cy.clearCookies();
     cy.clearLocalStorage();
-
-    // Perform the login before each test
-    cy.visit('https://.marshall-teach.web.id/');
-    cy.get('#username', timeout).clear().type('risna');
-    cy.get('#password', timeout).clear().type('telkomgoe');
-    cy.get('.form > .btn', timeout).click();
-    cy.wait(5000);
-    cy.url().should('include', '/dashboard'); // Verify that login is successful
   });
 
   it('Tes Generate Soal Coqa', () => {
-    // Pilih menu coqa
-    cy.wait(2000); // Jeda 2 detik
-    cy.get(':nth-child(5) > a > .fas', timeout).click();
+    // Mengunjungi URL aplikasi
+    cy.visit('https://.marshall-teach.web.id');
 
-    // Tunggu elemen grup kompetensi muncul dan bisa diklik
-    cy.wait(1000); // Jeda 1 detik
-    cy.get('.drop_grup > .css-13cymwt-control > .css-1wy0on6 > .css-1xc3v61-indicatorContainer > .css-8mmkcg')
-      .should('be.visible') // Pastikan elemen terlihat
-      .click(); // Klik elemen
-    cy.get('#react-select-4-option-0', timeout).click(); // Pilih opsi grup kompetensi
+    // Langkah Login
+    cy.get('#username', timeout).clear().type('risna'); // Mengisi username
+    cy.get('#password', timeout).clear().type('telkomgoe'); // Mengisi password
+    cy.wait(5000); // Menunggu 5 detik agar halaman dapat sepenuhnya dimuat
+    cy.get('.form > .btn', timeout).click(); // Menekan tombol login
 
-    // Pilih dirkom
-    cy.wait(1000); // Jeda 1 detik
-    cy.get('.drop_nama > .css-13cymwt-control > .css-lsbgp6')
-      .should('be.visible') // Pastikan elemen terlihat
-      .click();
-    cy.get('#react-select-5-option-1', timeout).click(); // Pilih opsi dirkom
+    // Memilih menu Coqa
+    cy.wait(5000); // Menunggu 5 detik agar navigasi stabil
+    cy.get(':nth-child(5) > a > .fas', timeout).click(); // Klik menu Coqa
+//========================================================================================================
+    // // Bagian 1: Generate Soal tanpa file upload
+    cy.get('.drop_grup > .css-58rwkm-control > .css-1khjq34 > .css-19bb58m', timeout)
+      .should('be.visible') // Pastikan elemen dropdown grup kompetensi terlihat
+      .click(); // Klik dropdown grup kompetensi
+    cy.get('#react-select-4-option-1', timeout).click(); // Pilih opsi grup kompetensi pertama
 
-    // Pilih level
-    cy.wait(1000); // Jeda 1 detik
+    cy.get('.drop_nama > .css-58rwkm-control > .css-1khjq34 > .css-19bb58m', timeout)
+      .should('be.visible') // Pastikan dropdown nama kompetensi terlihat
+      .click(); // Klik dropdown nama kompetensi
+    cy.get('#react-select-5-option-0', timeout).click(); // Pilih opsi kedua untuk nama kompetensi
+
     cy.get(':nth-child(9) > .Dropdown-root > .Dropdown-control > .Dropdown-placeholder', timeout)
-      .should('be.visible') // Pastikan elemen terlihat
-      .click();
-    cy.get('.Dropdown-menu > :nth-child(1)', timeout).click(); // Pilih opsi level
+      .should('be.visible') // Pastikan dropdown level terlihat
+      .click(); // Klik dropdown level
+    cy.get('.Dropdown-menu > :nth-child(3)', timeout).click(); // Pilih opsi level pertama
 
-    // Isi jumlah soal
-    cy.wait(1000); // Jeda 1 detik
-    cy.get('.inputText_coqa', timeout).clear().type('30');
+    cy.get('.inputText_coqa', timeout).clear().type('30'); // Mengisi jumlah soal
 
-    // Pilih database marshall
-    cy.wait(1000); // Jeda 1 detik
-    cy.get(':nth-child(1) > input', timeout).click();
+    cy.get(':nth-child(1) > input', timeout).click(); // Pilih opsi database marshall
 
-    // Pilih referensi
-    cy.wait(1000); // Jeda 1 detik
-    cy.get(':nth-child(1) > input', timeout).click();
-    //cy.get(':nth-child(2) > input').click();
+    cy.get('.dropdown_search_coqa > .css-58rwkm-control > .css-1khjq34 > .css-19bb58m', timeout)
+      .click(); // Klik dropdown referensi ebook
+    cy.get('[id^="react-select-"][id$="-option-2"]', timeout).click(); // Pilih opsi referensi ebook kedua
 
-    // Pilih multi select referensi ebook
-    cy.wait(1000); // Jeda 1 detik
-    cy.get('.dropdown_search_coqa > .css-13cymwt-control > .css-lsbgp6', timeout).click();
-    cy.get('[id^="react-select-"][id$="-option-8"]', timeout).click();
-
-    // Ambil screenshot
-    cy.wait(1000); // Jeda 1 detik
+    // Mengambil screenshot untuk dokumentasi proses pertama
     cy.screenshot(`generateCOQA-${new Date().toISOString().replace(/[:.]/g, '-')}`);
 
     // Klik tombol generate
-    cy.wait(1000); // Jeda 1 detik
+    cy.wait(5000); // Jeda 1 detik
     cy.get('.coqaBut', timeout).click();
+
+    //generate coqa baru
+    cy.wait(5000);
+    cy.get('.coqaBut_baru', timeout).click();
+
+
+//========================================================================================================
+    // Bagian 2: Generate Soal dengan file upload (PDF)
+    // cy.get('.drop_grup > .css-58rwkm-control > .css-1khjq34 > .css-19bb58m', timeout)
+    //   .should('be.visible') // Pastikan dropdown grup kompetensi terlihat
+    //   .click(); // Klik dropdown grup kompetensi
+    // cy.get('#react-select-4-option-1', timeout).click(); // Pilih opsi grup kompetensi pertama
+
+    // cy.get('.drop_nama > .css-58rwkm-control > .css-1khjq34 > .css-19bb58m', timeout)
+    //   .should('be.visible') // Pastikan dropdown nama kompetensi terlihat
+    //   .click(); // Klik dropdown nama kompetensi
+    // cy.get('#react-select-5-option-2', timeout).click(); // Pilih opsi kedua untuk nama kompetensi
+
+    // cy.get(':nth-child(9) > .Dropdown-root > .Dropdown-control > .Dropdown-placeholder', timeout)
+    //   .should('be.visible') // Pastikan dropdown level terlihat
+    //   .click(); // Klik dropdown level
+    // cy.get('.Dropdown-menu > :nth-child(2)', timeout).click(); // Pilih opsi level pertama
+
+    // cy.get('.inputText_coqa', timeout).clear().type('30'); // Mengisi jumlah soal
+
+    // cy.get('[style="display: flex; margin-top: 10px; font-size: 15px;"] > :nth-child(2) > input', timeout).click(); // Pilih opsi untuk upload file
+
+    // // Upload file PDF ke input file
+    // cy.get('.drop-file-input > input', timeout).attachFile('How To Use Consultative Selling For Revenue Growth.pdf');
+
+    // // Mengambil screenshot untuk dokumentasi proses kedua
+    // cy.screenshot(`generateCOQA-${new Date().toISOString().replace(/[:.]/g, '-')}`);
+
+    // // Klik tombol generate
+    // cy.wait(5000); // Jeda 1 detik
+    // cy.get('.coqaBut', timeout).click();
+
+    // //generate coqa baru
+    // cy.wait(5000);
+    // cy.get('.coqaBut_baru', timeout).click();
+
+//========================================================================================================
+    // // Bagian 3: Generate Soal dengan file upload (PPTX)
+    // cy.get('.drop_grup > .css-58rwkm-control > .css-1khjq34 > .css-19bb58m', timeout)
+    //   .should('be.visible') // Pastikan dropdown grup kompetensi terlihat
+    //   .click(); // Klik dropdown grup kompetensi
+    // cy.get('#react-select-4-option-9', timeout).click(); // Pilih opsi grup kompetensi pertama
+
+    // cy.get('.drop_nama > .css-58rwkm-control > .css-1khjq34 > .css-19bb58m', timeout)
+    //   .should('be.visible') // Pastikan dropdown nama kompetensi terlihat
+    //   .click(); // Klik dropdown nama kompetensi
+    // cy.get('#react-select-5-option-2', timeout).click(); // Pilih opsi kedua untuk nama kompetensi
+
+    // cy.get(':nth-child(9) > .Dropdown-root > .Dropdown-control > .Dropdown-placeholder', timeout)
+    //   .should('be.visible') // Pastikan dropdown level terlihat
+    //   .click(); // Klik dropdown level
+    // cy.get('.Dropdown-menu > :nth-child(3)', timeout).click(); // Pilih opsi level pertama
+
+    // cy.get('.inputText_coqa', timeout).clear().type('30'); // Mengisi jumlah soal
+
+    // cy.get('[style="display: flex; margin-top: 10px; font-size: 15px;"] > :nth-child(2) > input', timeout).click(); // Pilih opsi untuk upload file
+    // cy.get(':nth-child(1) > .Dropdown-root > .Dropdown-control > .Dropdown-placeholder').click() //pilih pptx
+
+    // // Upload file PDF ke input file
+    // cy.get('.drop-file-input > input', timeout).attachFile('[213_Data Engineering]_Pengantar Dasar Data Engineering dan Siklus Hidup Data_fundamental.pptx');
+
+    // // Mengambil screenshot untuk dokumentasi proses kedua
+    // cy.screenshot(`generateCOQA-${new Date().toISOString().replace(/[:.]/g, '-')}`);
+
+    // // Klik tombol generate
+    // cy.wait(5000); // Jeda 1 detik
+    // cy.get('.coqaBut', timeout).click();
+
   });
 });
